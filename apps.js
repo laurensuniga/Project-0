51c146cbf5205e81bfb4600a7dcc1c0517d9951b
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2020-09-01 12:45:53 -0700

    js plan made

---
+++ apps.js
@@ -0,0 +1,19 @@
+// create and array for all the cards
+// 52 different cards
+
+
+// create a deck of cards- generates deck from the array of cards
+// shuffle
+// draws a card from the draw pile
+// discard - removes a card and puts it in the card played pile
+
+
+// Player
+// draws a card
+
+
+// Game
+// start function
+// compairs card to choose winner
+// a war/battle funtion
+// a deal cards function
